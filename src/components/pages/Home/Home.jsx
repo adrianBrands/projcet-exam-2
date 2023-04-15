b1ae32780721152e3e6c65cb1e5f5cbe7923cba6
adrianBrands <adrianbrandshaug@hotmail.com>
2023-04-15 09:01:40 +0200

    added content on home page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,10 @@
+import { Button, Container } from "react-bootstrap"
 import background from "../../../images/china-gf98bd0a45_1920.jpg"
 export default function Home(){
-  return <div className="backgroundImage" > yello</div>
+  return <div className="backgroundImage">
+          <Container>
+            <h1 className="text-white fst-italic fw-normal mt-5">Stay once, carry memories forever...</h1>
+            <Button size="lg" className="mt-3">Find Your Stay</Button>
+          </Container>
+         </div>
 }
