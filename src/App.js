d8dfd7908aad76b06fbd122a15989ea82c303965
adrianBrands <adrianbrandshaug@hotmail.com>
2023-04-15 07:35:20 +0200

    seting up routing and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Header, Footer } from "./components";
+import { Home, Venues } from "./components/pages";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
       <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/venues" element={<Venues />}/>
+      </Routes>
       <Footer/>
     </>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,30 +3,6 @@ import { NavLink, Link } from "react-router-dom";
 import Logo from "../../images/holidaze1.png"
 
 
-/*export default function Header() {
- 
-  return ( 
-    <Navbar sticky="top" className="bg-white shadow-sm mb-3 border-bottom border-primary">
-      <Container>
-      
-       <div className="d-flex flex-column align-items-center justify-content-center">
-        <NavbarBrand href="/">Holidaze</NavbarBrand>
-        <p className="border-top">Your next Space</p>
-       </div> 
-      
-        <Nav className=" fs-5">
-        
-          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
-          <Nav.Link to="/contact" as={NavLink}>Contact</Nav.Link>
-        </Nav>
-        
-        
-        <div>hell9</div>
-      </Container>
-    </Navbar>
-  );
-}*/
-
 export default function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" className="border-bottom border-primary d-flex">
@@ -39,11 +15,11 @@ export default function Header() {
               alt="holidaze logo"
             />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse className="ms-3" id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#features">Home</Nav.Link>
-          <Nav.Link href="#pricing">Venues</Nav.Link>
+          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+          <Nav.Link to="/venues" as={NavLink}>Venues</Nav.Link>
         </Nav>
         <Nav className="me-3">
           <Nav.Link href="#deets">More deets</Nav.Link>
